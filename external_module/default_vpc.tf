4aaaf3a1b8c5cddfdbd8acad65cb7a2ca04c8748
Devon7272 <drob678@gmail.com>
2020-10-28 09:30:46 -0500

    Update default_vpc.tf

--- external_module/default_vpc.tf
+++ external_module/default_vpc.tf
@@ -1,15 +1,18 @@
 # these resources are added to provide the vpc_id and subnets to the consul module
 # aws_default_* are resources that terraform doesn't creat, rather adopt
 # See https://www.terraform.io/docs/providers/aws/r/default_vpc.html for more info
-
-# default VPC
+##############
+# default VPC#
+##############
 resource "aws_default_vpc" "default" {
   tags = {
     Name = "Default VPC"
   }
 }
 
-# default subnets
+##################
+# default subnets#
+##################
 resource "aws_default_subnet" "default_az1" {
   availability_zone = "${var.AWS_REGION}a"
 
